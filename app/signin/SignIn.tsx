361b7cd2ddda34f39cd268775a078f0c148a32e2
Abdulrahman <abd3lra7man.078@gmail.com>
2024-12-23 13:34:44 +0300

    initial commit.

---
+++ app/signin/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <main className={`w-full min-h-screen`}>
+            <div className={``}>
+
+            </div>
+        </main>
+    );
+};
+
+export default SignIn;
